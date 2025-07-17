57cd0c94d73670966fdc3039d2024797bc79010d
Priyanshu1-62 <bpriyanshu5678@gmail.com>
2025-07-18 00:43:18 +0530

    Update CORS origin for production deployment

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://i-note-book-two.vercel.app',
   credentials: true
 }));
 app.use(express.json());
